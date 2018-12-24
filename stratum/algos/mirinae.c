520ca1bd4a7b4c347b7f8094eecef46394247c18
iamstenman <39154602+itwysgsl@users.noreply.github.com>
2018-12-24 16:31:45 +0200

    Update mirinae.c

--- stratum/algos/mirinae.c
+++ stratum/algos/mirinae.c
@@ -12,81 +12,40 @@
 #include "mirinae.h"
 #include <memory.h>
 
-/*
-#include <chain.h>
-#include <microbitcoin.h>
-#include <validation.h>
-
-int GetBlockHeight(const CBlockHeader &block) {
-    return GetBlockHeight(block.hashPrevBlock);
-}
-
-int GetBlockHeight(const uint256 &hashPrevBlock) {
-    CBlockIndex *pindexPrev = NULL;
-    int nHeight = 0;
-
-    BlockMap::iterator mi = mapBlockIndex.find(hashPrevBlock);
-    if (mi != mapBlockIndex.end()) {
-        pindexPrev = (*mi).second;
-        nHeight = pindexPrev->nHeight + 1;
-    }
-
-    return nHeight;
-}
-*/
-
-// need SEED
-
-//void mirinae(const void* data, size_t length, void* output, int height, const void* seed)
-void mirinae_hash(const char* input, char* output, uint32_t len)
+void mirinae_hash(const void* input, void* output, size_t length, int height, const void* seed)
 {
-	void *in = (void*) input;
-        int size = 80;
-
-	//seed
-
-	unsigned char hash1[64];
-	unsigned char hash2[64];
-
-	//unsigned char hash[64] = { 0 };
-	//unsigned char offset[64] = { 0 };
-	//const int window = 4096;
-	//const int aperture = 32;
-	//int64_t n = 0;
+	unsigned char hash[64] = { 0 };
+	unsigned char offset[64] = { 0 };
+	const int window = 4096;
+	const int aperture = 32;
+	int64_t n = 0;
 
 	sph_groestl512_context ctx_groestl;
 	struct kupyna512_ctx_t ctx_kupyna;
-	//kupyna512_context ctx_kupyna;
 
-	//kupyna512_init(&ctx_kupyna);
-	//kupyna512_update(&ctx_kupyna, seed, 32); /* seed -> ? */
-	//kupyna512_final(&ctx_kupyna, offset);
-
-	//memcpy(&n, offset, 8);
+	kupyna512_init(&ctx_kupyna);
+	kupyna512_update(&ctx_kupyna, seed, 32);
+	kupyna512_final(&ctx_kupyna, offset);
+	memcpy(&n, offset, 8);
 
 	sph_groestl512_init(&ctx_groestl);
-	sph_groestl512(&ctx_groestl, in, size); /* data -> input */
-	sph_groestl512_close(&ctx_groestl, hash1);
+	sph_groestl512(&ctx_groestl, input, length);
+	sph_groestl512_close(&ctx_groestl, hash);
 
-	/*
 	unsigned int h_loop = hash[0];
-
-	for (int i = 0; i < (((n % nHeight) + (height + 1)) % window); i++) {
+	for (int i = 0; i < (((n % height) + (height + 1)) % window); i++) {
 		for (int j = 0; j < (h_loop % aperture); j++) {
-	*/
 			kupyna512_init(&ctx_kupyna);
-			kupyna512_update(&ctx_kupyna, hash1, 64);
-			kupyna512_final(&ctx_kupyna, hash2);
-	/*
+			kupyna512_update(&ctx_kupyna, hash, 64);
+			kupyna512_final(&ctx_kupyna, hash);
 		}
 
-		h_loop = hash2[0];
+		h_loop = hash[0];
 	}
-	*/
 
 	sph_groestl512_init(&ctx_groestl);
-	sph_groestl512(&ctx_groestl, hash2, 64);
-	sph_groestl512_close(&ctx_groestl, hash1);
+	sph_groestl512(&ctx_groestl, hash, 64);
+	sph_groestl512_close(&ctx_groestl, hash);
 
-	memcpy(output, hash1, 32);
+	memcpy(output, hash, 32);
 }
